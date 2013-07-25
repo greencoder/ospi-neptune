568c0f93ec44f1a9375ff8aa6918b1575be24621
Scott Newman <snewman18@gmail.com>
2013-07-24 18:40:24 -0600

    Big update. Moved host and port settings to config file. Changed create_delay to be a server function. Changed delay handling to automatically check for expired delays.

--- operate.py
+++ operate.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import argparse
+import json
 
 import config
 
@@ -10,16 +11,17 @@ parser.add_argument('--station', type=int, help='Station to run [1-8]', required
 parser.add_argument('--minutes', type=int, help='Number of minutes to run station.', required=True)
 args = vars(parser.parse_args())
 
-station_number = args['station']
-number_minutes = args['minutes']
+station = args['station']
+minutes = args['minutes']
 
-if station_number not in range(0, config.NUMBER_OF_STATIONS):
+if station not in range(0, config.NUMBER_OF_STATIONS):
     sys.exit("Error: Invalid Station Number. Must be 0 to %d." % config.NUMBER_OF_STATIONS)
 
-if number_minutes not in range(0, config.MAX_MINUTES_PER_STATION):
+if minutes not in range(0, config.MAX_MINUTES_PER_STATION):
     sys.exit("Error: The number of minutes exceeds what is allowed in the configuration.")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 9999))
-sock.sendall("%s,%s" % (station_number, number_minutes))
+sock.connect((config.HOST, config.PORT))
+cmd = { 'cmd': 'operate-station', 'args': { 'station': station, 'minutes': minutes } }
+sock.sendall("%s" % json.dumps(cmd))
 sock.close()
--- utilities/create_delay.py
+++ utilities/create_delay.py
@@ -2,23 +2,26 @@ import os
 import sys
 import argparse
 import datetime
-
-# This file is just a convenience to automatically write out the DELAY
-# file with the proper datetime expiration string as the body.
+import json
+import socket
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT_DIR = os.path.abspath(os.path.join(CUR_DIR, os.pardir))
+
+# Add the parent dir to the search path so we can import config
+sys.path.insert(0, PARENT_DIR)
+import config
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--hours', help='# of hours to pause job execution', type=int, required=True)
 
 args = vars(parser.parse_args())
 
-# Calculate what the datetime object will be by adding the current time
-# and the number of hours to delay. This will be the body of the DELAY file.
-future_time = datetime.datetime.now() + datetime.timedelta(hours=args['hours'])
-
-# Write out the DELAY file and make the body the expiration time.
-f = open(os.path.join(PARENT_DIR, 'DELAY'), 'w')
-f.write(future_time.strftime('%Y-%m-%d %H:%M'))
-f.close()
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((config.HOST, config.PORT))
+    cmd = { 'cmd': 'create-delay', 'args': { 'hours': args['hours'] }}
+    sock.sendall("%s" % json.dumps(cmd))
+    sock.close()
+except socket.error:
+    sys.exit("Could not connect to network socket.")
